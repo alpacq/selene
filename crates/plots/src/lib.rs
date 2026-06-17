b317b599e933185099827628e4bb4defaca66749
Krzysztof Lam <krlam@gnhearing.com>
2026-06-17 15:01:08 +0200

    Plots crate to handle plotting

---
+++ crates/plots/src/lib.rs
@@ -0,0 +1,87 @@
+use nalgebra::DVector;
+use plotters::prelude::*;
+
+fn plot(
+    x_axis_values: Vec<f64>,
+    y_axis_values: Vec<f64>,
+    x_axis_label: String,
+    y_axis_label: String,
+    title: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let name = format!("{title}.png");
+
+    let x_min = x_axis_values
+        .iter()
+        .map(|x| *x)
+        .fold(f64::INFINITY, f64::min)
+        - 1.0;
+    let x_max = x_axis_values
+        .iter()
+        .map(|x| *x)
+        .fold(f64::NEG_INFINITY, f64::max)
+        + 1.0;
+    let y_min = y_axis_values
+        .iter()
+        .map(|y| *y)
+        .fold(f64::INFINITY, f64::min)
+        - 1.0;
+    let y_max = y_axis_values
+        .iter()
+        .map(|y| *y)
+        .fold(f64::NEG_INFINITY, f64::max)
+        + 1.0;
+
+    let root = BitMapBackend::new(name.as_str(), (1024, 768)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption(title, ("sans-serif", 24).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
+
+    chart
+        .configure_mesh()
+        .x_desc(x_axis_label)
+        .y_desc(y_axis_label)
+        .draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            x_axis_values
+                .iter()
+                .zip(y_axis_values.iter())
+                .map(|(x, y)| (*x, *y)),
+            &BLACK,
+        ))?
+        .label("x2(x1)")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
+}
+
+pub fn phase_portait(
+    data: Vec<DVector<f64>>,
+    title: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let x_axis_values = data.iter().map(|d| d[1]).collect::<Vec<f64>>();
+    let y_axis_values = data.iter().map(|d| d[2]).collect::<Vec<f64>>();
+    let x_axis_label = "x1".into();
+    let y_axis_label = "x2".into();
+
+    plot(
+        x_axis_values,
+        y_axis_values,
+        x_axis_label,
+        y_axis_label,
+        title,
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use math::{input::Input, state::State, timestep::TimeStep};
 use nalgebra::dvector;
-use plotters::prelude::*;
+use plots::phase_portait;
 use sim::run;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -12,31 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &TimeStep::new(0.001),
     )?;
 
-    let root = BitMapBackend::new("plot.png", (1024, 768)).into_drawing_area();
-    root.fill(&WHITE)?;
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Van der Pol Oscillator", ("sans-serif", 24).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(-3f64..3f64, -4f64..5f64)?;
-
-    chart.configure_mesh().x_desc("x1").y_desc("x2").draw()?;
-
-    chart
-        .draw_series(LineSeries::new(output.iter().map(|o| (o[1], o[2])), &BLACK))?
-        .label("x2(x1)")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
-
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
-
-    root.present()?;
-
-    Ok(())
+    phase_portait(output, "Van der Pol Oscillator".into())
 }
 
 pub fn van_der_pol(x: &State, u: &Input) -> State {
